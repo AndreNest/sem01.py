bf08bbeca4c1a2fb9727cb2f365983659e81472d
AndreyNesterov <1@nestandre.ru>
2022-11-22 23:49:57 +0300

    task_02 complite

--- task_02.py
+++ task_02.py
@@ -4,3 +4,23 @@
 #
 # - 1, 4, 8, 7, 5 -> 8
 # - 78, 55, 36, 90, 2 -> 90
+import sys
+
+
+def get_number(input_string):
+    try:
+        num = int(input(input_string))
+        return  num
+    except(ValueError):
+        print('Ошибка ввода! Введите число.')
+        return get_number(input_string)
+
+def max_num(x):
+    max_numb = -sys.maxsize
+    for i in range(x):
+        num = int(get_number('Введите число: '))
+        if num > max_numb:
+            max_numb = num
+    print(f'Максимальное число = {max_numb}')
+
+max_num(get_number('сколько чисел будет введено? '))
