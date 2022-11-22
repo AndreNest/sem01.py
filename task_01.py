b1f741599c332f141b5988b26cbaae78ab3987aa
AndreyNesterov <1@nestandre.ru>
2022-11-22 23:16:42 +0300

    update

--- task_01.py
+++ task_01.py
@@ -2,3 +2,14 @@ num_1 = 10
 num_2 = 15
 num_3 = num_1 + num_2
 print(num_3)
+
+
+print(num_1, num_2)
+
+num_1, num_2 = num_2, num_1
+
+print(num_1, num_2)
+
+num_1 = num_2 = 15
+
+print(num_1, num_2)
