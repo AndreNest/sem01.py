84f0c6a9015685060aefcd618320967ffb14458a
AndreyNesterov <nest.ande@yandex.ru>
2022-11-25 14:50:51 +0300

    update

--- task_09.py
+++ task_09.py
@@ -3,9 +3,25 @@
 # После отсчета срабатывает будильник
 import time
 text = str(input('О чем вам напомнить? '))
-local_time_hours = float(input('Через сколько часов?'))
-local_time_min = float(input('Через сколько минут?'))
-local_time_sec = float(input('Через сколько секунд?'))
+local_time_hours = int(input('Через сколько часов?'))
+local_time_min = int(input('Через сколько минут?'))
+local_time_sec = int(input('Через сколько секунд?'))
 local_time_total = (local_time_hours * 60 * 60) + (local_time_min * 60) + local_time_sec
-time.sleep(local_time_total)
-print(text)
+def timer(time):
+    while time > 0:
+        hour = 0
+        min = 0
+        sec = 0
+        if time > 60 * 60:
+            hour = time // 3600
+            time = time % 3600
+        if time > 60:
+            min = time // 60
+            time = time % 60
+        if time > 0:
+            print(f'осталось {time}  секунд!!!!')
+            time.sleep(1)
+            time -= 1
+    print(text)
+
+timer(local_time_total)
